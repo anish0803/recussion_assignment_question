10e8ef96e3d196d0e9c4d57ad1d4cf3a2688f5cc
anish0803 <singhanish124@gmail.com>
2022-06-24 23:25:03 +0530

    sum all given digit no.

--- q6.js
+++ q6.js
@@ -1,11 +1,24 @@
 // -------------Print sum of all the numbers in the given range--------------
 
-function sum_range(num1,num2,total){ 
-    if(num1<=num2){ 
-           total=total+num1; 
-          return sum_range(num1+1,num2,total); 
-     } 
-      return total; 
-   } 
-    console.log(sum_range(1,10,0));
+// function sum_range(num1,num2,total){ 
+//     if(num1<=num2){ 
+//            total=total+num1; 
+//           return sum_range(num1+1,num2,total); 
+//      } 
+//       return total; 
+//    } 
+//     console.log(sum_range(1,10,0));
+
+
+
+
+
+    function sum_range(num1,num2){ 
+        if(num1>num2){ 
+            return 0;
+             
+         } 
+          return num1+sum_range(num1+1,num2); 
+       } 
+        console.log(sum_range(1,10));
   
